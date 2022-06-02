c75c9c0e6b57ea82661ae820a0216ae5178b9585
joaobnd <joao.martins@ccc.ufcg.edu.br>
2022-06-02 16:57:36 -0300

    test create account and load account

--- src/data/services/facebook-athentication.ts
+++ src/data/services/facebook-athentication.ts
@@ -1,14 +1,23 @@
 import { LoadFacebookUserApi } from '@/data/contracts/apis'
+import { LoadUserAccountRepository, CreateFacebookAccountRepository } from '@/data/contracts/repos'
 import { AuthenticationError } from '@/domain/errors'
 import { FacebookAuthentication } from '@/domain/features'
 
 export class FacebookAuthenticationService {
-  constructor (private readonly loadFacebookUserApi: LoadFacebookUserApi) {
+  constructor (
+    private readonly loadFacebookUserApi: LoadFacebookUserApi,
+    private readonly loadUserAccountRepo: LoadUserAccountRepository,
+    private readonly createFacebookAccountRepo: CreateFacebookAccountRepository
+  ) {
 
   }
 
   async execute (params: FacebookAuthentication.Params): Promise<AuthenticationError> {
-    await this.loadFacebookUserApi.loadUser(params)
+    const fbData = await this.loadFacebookUserApi.loadUser(params)
+    if (fbData !== undefined) {
+      await this.loadUserAccountRepo.load({ email: fbData.email })
+      await this.createFacebookAccountRepo.createFromFacebook(fbData)
+    }
     return new AuthenticationError()
   }
 }
